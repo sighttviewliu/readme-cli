88f9a31050e39b3a7c65d92faf0a0431ec78f5f3
Gregory Koberger <gkoberger@gmail.com>
2014-08-27 19:59:04 -0700

    Package as NPM module

---
+++ README.md
@@ -0,0 +1,1 @@
+Upload your docs to readme.io. Currently in super-beta. Email cli@readme.io for help.
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-#!/usr/bin/env node
-
-/* TODO:
- * [ ] Better checking of options?
- */
-
 var program = require('commander')
-
   , deploy = require('./lib/deploy')
 
-program
+function convert() {
+  program
   .version('0.0.1')
   .usage('readmeio deploy [version]')
   .option('-v, --verbose', 'Verbose output')
   .parse(process.argv);
 
-if(program.args[0] == 'deploy') {
-  deploy.exec(program);
-} else {
-  program.help();
-}
+  if(program.args[0] == 'deploy') {
+    deploy.exec(program);
+  } else {
+    program.help();
+  }
+};
 
+exports.convert = convert;
+
+/* TODO:
+ * [ ] Better checking of options?
+ */
 
